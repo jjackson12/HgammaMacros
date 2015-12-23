36931349abbc8e139ec76d52d3f053758dd0cf9a
John Hakala <john_hakala@brown.edu>
2015-12-23 14:33:11 -0500

    improve treeChecker pyroot runner macro

--- runTreeChecker.py
+++ runTreeChecker.py
@@ -1,15 +1,34 @@
+# Macro to check a tree in the WZgamma ntuples
+# This macro takes either 0 or 1 arguments:
+# With zero arguments, it will check hardcoded default tree
+# With one argument, the tree that will be checked is the "ntuplizer/tree" tree
+# in the root file specified in the argument
+# Example: 
+# python runTreeChecker.py myWZgammaNtuple.root
+
 from ROOT import *
 import os, subprocess
+from sys import argv
+
+# function to compile a C/C++ macro for loading into a pyroot session
 def compileMacro(macroName):
+        # remove the previously compiled libraries
 	if os.path.exists(macroName+"_C_ACLiC_dict_rdict.pcm"):
     		os.remove(macroName+"_C_ACLiC_dict_rdict.pcm")
 	if os.path.exists(macroName+"_C.d"):
     		os.remove(macroName+"_C.d")
 	if os.path.exists(macroName+"_C.so"):
     		os.remove(macroName+"_C.so")
+        # compile the macro using g++
 	macro = macroName + ".C++g"
 	subprocess.call(["root", "-l", "-q", macro])
+
+# get the tree specified by argument 1
+file = TFile(argv[1])
+tree = file.Get("ntuplizer/tree")
+
+# call the compiling function to compile the treeChecker, then run its Loop() method
 compileMacro("treeChecker")
 gSystem.Load('treeChecker_C')
-checker = treeChecker()
+checker = treeChecker(tree)
 checker.Loop()
