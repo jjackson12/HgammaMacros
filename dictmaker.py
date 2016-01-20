07a63abe9b515bd4c8f9d0c2833253070fe34b41
John Hakala <john_hakala@brown.edu>
2016-01-20 19:27:08 +0100

    check for input files in dictmaker

--- dictmaker.py
+++ dictmaker.py
@@ -1,7 +1,10 @@
+from os import path
 def makeInputsDict(inputsListFile):
 	inputsList = open(inputsListFile)
 	inputsDict = dict()
 	for line in inputsList:
 		lineParts = line.split()
+		if not path.exists(lineParts[1]):
+			exit("\nError: file %s not found.\n"%lineParts[1])
 		inputsDict[lineParts[0]] = lineParts[1]
 	return inputsDict
