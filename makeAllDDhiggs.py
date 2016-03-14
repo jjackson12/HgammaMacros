b6ae8c92ee3f6fa2fbecf79c55c37ff666217c28
John Hakala <john_hakala@brown.edu>
2016-03-14 10:10:45 +0100

    add newest scripts

--- makeAllDDhiggs.py
+++ makeAllDDhiggs.py
@@ -1,27 +1,36 @@
-import subprocess
 from ROOT import *
 from sys import argv
-argsList =  [
-              ["leadingPhPt"               , "leadingPhPt"                     ]  ,
-              ["leadingPhAbsEta"           , "leadingPhAbsEta"                 ]  ,
-              ["cosThetaStar"              , "cosThetaStar"                    ]  ,
-              ["phPtOverMgammaj"           , "phPtOverMgammaj"                 ]
-            ]
-if argv[1]=="50to70":
-    argsList.append(["phJetDeltaR_higgs"           , "phJetDeltaR_sideLowThree"          ]  )
-    argsList.append(["phJetInvMass_pruned_higgs"   , "phJetInvMass_pruned_sideLowThree"  ]  )
-    argsList.append(["higgsJet_pruned_abseta"      , "sideLowThreeJet_pruned_abseta"     ]  )
-    argsList.append(["higgsJett2t1"                , "sideLowThreeJett2t1"               ]  )
-    argsList.append(["higgsJet_pruned_abseta"      , "sideLowThreeJet_pruned_abseta"     ]  )
-elif argv[1]=="100to110":
-    argsList.append(["phJetDeltaR_higgs"           , "phJetDeltaR_sideLowFour"           ]  )
-    argsList.append(["phJetInvMass_pruned_higgs"   , "phJetInvMass_pruned_sideLowFour"   ]  )
-    argsList.append(["higgsJet_pruned_abseta"      , "sideLowFourJet_pruned_abseta"      ]  )
-    argsList.append(["higgsJett2t1"                , "sideLowFourJett2t1"                ]  )
-    argsList.append(["higgsJet_pruned_abseta"      , "sideLowFourJet_pruned_abseta"      ]  )
-else:
-    exit("please pick sideband, 50to70 or 100to110")
+import makeDDhiggs as dd
 
-for args in argsList:
-    subprocess.call(["python", "makeDDhiggs.py", args[0], args[1], argv[1], "-b"])
-exit()
+canvases1=[]
+canvases2=[]
+sidebands = ["50to70","100to110"]
+for sideband in sidebands:
+    print sideband
+    argsList =  [
+                  ["leadingPhPt"               , "leadingPhPt"                     ]  ,
+                  ["leadingPhAbsEta"           , "leadingPhAbsEta"                 ]  ,
+                  ["cosThetaStar"              , "cosThetaStar"                    ]  ,
+                  ["phPtOverMgammaj"           , "phPtOverMgammaj"                 ]
+                ]
+    if sideband=="50to70":
+        argsList.append(["phJetDeltaR_higgs"           , "phJetDeltaR_sideLowThree"          ]  )
+        argsList.append(["phJetInvMass_pruned_higgs"   , "phJetInvMass_pruned_sideLowThree"  ]  )
+        argsList.append(["higgsJet_pruned_abseta"      , "sideLowThreeJet_pruned_abseta"     ]  )
+        argsList.append(["higgsJett2t1"                , "sideLowThreeJett2t1"               ]  )
+        argsList.append(["higgsJet_pruned_abseta"      , "sideLowThreeJet_pruned_abseta"     ]  )
+    elif sideband=="100to110":
+        argsList.append(["phJetDeltaR_higgs"           , "phJetDeltaR_sideLowFour"           ]  )
+        argsList.append(["phJetInvMass_pruned_higgs"   , "phJetInvMass_pruned_sideLowFour"   ]  )
+        argsList.append(["higgsJet_pruned_abseta"      , "sideLowFourJet_pruned_abseta"      ]  )
+        argsList.append(["higgsJett2t1"                , "sideLowFourJett2t1"                ]  )
+        argsList.append(["higgsJet_pruned_abseta"      , "sideLowFourJet_pruned_abseta"      ]  )
+    else:
+        exit("please pick sideband, 50to70 or 100to110 here")
+
+    for args in argsList:
+        if sideband=="50to70":
+            dd.makeDDhiggs(args[0], args[1], sideband, argv[1], argv[2])
+        if sideband=="100to110":
+            dd.makeDDhiggs(args[0], args[1], sideband, argv[1], argv[2])
+
--- makeDDhiggs.py
+++ makeDDhiggs.py
@@ -1,128 +1,275 @@
-from sys import argv
 from os import path, makedirs
 import subprocess
 from math import sqrt
 from ROOT import *
 
-dataFile = TFile("ddTreeHiggs_silver_Feb26.root")
+def makeDDhiggs(argv1, argv2, argv3, argv4, argv5):
+    argv=[ "leethax", argv1, argv2, argv3, argv4, argv5]
+    print argv
+    cosThetaCut   = True;
+    compareOldCut = False;
+    showSignal    = True;
 
-data          = dataFile.Get("higgs"     )
-print "using sideband %s"%argv[3]
-if (argv[3]=="100to110"):
-    sidebandName = "side100110"
-    sidebandIndex = "Four"
-elif (argv[3]=="50to70"):
-    sidebandName = "side5070"
-    sidebandIndex = "Three"
-else:
-    exit("please pick the sideband range: either 100to110 or 50to70.")
-sideLow = dataFile.Get(sidebandName)
+    dataFile = TFile("../HgammaSamples/ddTrees/ddNew_silver.root")
+    sig750File   = TFile("../HgammaSamples/ddTrees/ddNew_Hgamma_m750.root")
+    sig1000File  = TFile("../HgammaSamples/ddTrees/ddNew_Hgamma_m1000.root")
+    sig2000File  = TFile("../HgammaSamples/ddTrees/ddNew_Hgamma_m2000.root")
+    sig3000File  = TFile("../HgammaSamples/ddTrees/ddNew_Hgamma_m3000.root")
 
-phoEtaMax   = 1.4442
-jetEtaMax   = 2.0
-deltaRmin   = 1.1
-cosThetaMax = 0.6
+    trees = []
+    data          = dataFile.Get(   "higgs"  )
+    trees.append(data)
+    sig750        = sig750File.Get( "higgs"  )
+    trees.append(sig750)
+    sig1000       = sig1000File.Get("higgs"  )
+    trees.append(sig1000)
+    sig2000       = sig2000File.Get("higgs"  )
+    trees.append(sig2000)
+    sig3000       = sig3000File.Get("higgs"  )
+    trees.append(sig3000)
+    for tree in trees:
+        print tree
 
-massWindowLo = 600
-massWindowHi = 700
+    print "using sideband %s"%argv[3]
+    if (argv[3]=="100to110"):
+        sidebandName = "side100110"
+        sidebandIndex = "Four"
+    elif (argv[3]=="50to70"):
+        sidebandName = "side5070"
+        sidebandIndex = "Three"
+    else:
+        exit("please pick the sideband range: either 100to110 or 50to70.")
+    sideLow = dataFile.Get(sidebandName)
 
-phoEtaCut           = TCut("leadingPhAbsEta<%s"               % str(phoEtaMax  ) )
-cosThetaCut         = TCut("cosThetaStar<%s"                  % str(cosThetaMax) )
-deltaRdataCut       = TCut("phJetDeltaR_higgs>%s"             % str(deltaRmin  ) )
-deltaRsidebandCut   = TCut("phJetDeltaR_sideLow%s>%s"         % (sidebandIndex, str(deltaRmin)) )
-jetEtaDataCut       = TCut("higgsJet_pruned_abseta<%s"        % str(jetEtaMax  ) )
-jetEtaSidebandCut   = TCut("sideLow%sJet_pruned_abseta<%s"  % (sidebandIndex, str(jetEtaMax)) )
+    phoEtaMax   = 1.4442
+    jetEtaMax   = 2.0
+    deltaRmin   = 1.1
+    cosThetaMax = 0.6
 
-dataMassCuts         = TCut("(phJetInvMass_pruned_higgs>%s)&&(phJetInvMass_pruned_higgs<%s)"               % ( str(massWindowLo), str(massWindowHi) )    )
-sidebandMassCuts     = TCut("(phJetInvMass_pruned_sideLow%s>%s)&&(phJetInvMass_pruned_sideLow%s<%s)" % (sidebandIndex, str(massWindowLo), sidebandIndex, str(massWindowHi) )    )
+    massWindowLo = int(argv[4])
+    massWindowHi = int(argv[5])
 
-dataCuts     = TCut()
-sidebandCuts = TCut()
+    phoEtaCut           = TCut("leadingPhAbsEta<%s"               % str(phoEtaMax  ) )
+    cosThetaCut         = TCut("cosThetaStar<%s"                  % str(cosThetaMax) )
+    deltaRdataCut       = TCut("phJetDeltaR_higgs>%s"             % str(deltaRmin  ) )
+    deltaRsidebandCut   = TCut("phJetDeltaR_sideLow%s>%s"         % (sidebandIndex, str(deltaRmin)) )
+    jetEtaDataCut       = TCut("higgsJet_pruned_abseta<%s"        % str(jetEtaMax  ) )
+    jetEtaSidebandCut   = TCut("sideLow%sJet_pruned_abseta<%s"  % (sidebandIndex, str(jetEtaMax)) )
+    cosThetadataCut     = TCut("cosThetaStar<0.6+0.1*(phJetInvMass_pruned_higgs-750)/250"                                          )
+    cosThetasidebandCut = TCut("cosThetaStar<0.6+0.1*(phJetInvMass_pruned_sideLow%s-750)/250"  % sidebandIndex                   )
+    cosThetadataOldCut     = TCut("cosThetaStar<0.6"                                          )
+    cosThetasidebandOldCut = TCut("cosThetaStar<0.6"                   )
 
-dataCuts     += phoEtaCut + cosThetaCut + deltaRdataCut     + jetEtaDataCut     + dataMassCuts
-sidebandCuts += phoEtaCut + cosThetaCut + deltaRsidebandCut + jetEtaSidebandCut + sidebandMassCuts
+    dataMassCuts         = TCut("(phJetInvMass_pruned_higgs>%s)&&(phJetInvMass_pruned_higgs<%s)"               % ( str(massWindowLo), str(massWindowHi) )    )
+    sidebandMassCuts     = TCut("(phJetInvMass_pruned_sideLow%s>%s)&&(phJetInvMass_pruned_sideLow%s<%s)" % (sidebandIndex, str(massWindowLo), sidebandIndex, str(massWindowHi) )    )
+
+    dataCuts     = TCut()
+    sidebandCuts = TCut()
+    dataOldCuts     = TCut()
+    sidebandOldCuts = TCut()
+
+    if cosThetaCut:
+        dataCuts     += phoEtaCut + cosThetadataCut + deltaRdataCut     + jetEtaDataCut     + dataMassCuts
+        sidebandCuts += phoEtaCut + cosThetasidebandCut + deltaRsidebandCut + jetEtaSidebandCut + sidebandMassCuts
+        dataOldCuts     += phoEtaCut + cosThetadataOldCut + deltaRdataCut     + jetEtaDataCut     + dataMassCuts
+        # These can be swapped to compare old cosTheta cut vs. no cosTheta cut
+        #sidebandOldCuts += phoEtaCut + cosThetasidebandOldCut + deltaRsidebandCut + jetEtaSidebandCut + sidebandMassCuts
+        sidebandOldCuts += phoEtaCut + deltaRsidebandCut + jetEtaSidebandCut + sidebandMassCuts
+    else:
+        dataCuts     += phoEtaCut + deltaRdataCut     + jetEtaDataCut     + dataMassCuts
+        sidebandCuts += phoEtaCut + deltaRsidebandCut + jetEtaSidebandCut + sidebandMassCuts
 
 
-print "the cut string for the signal region is %s" % dataCuts
-print "the cut string for the sideband region is %s" % sidebandCuts
+    print "the cut string for the signal region is %s" % dataCuts
+    print "the cut string for the sideband region is %s" % sidebandCuts
 
 
-c = TCanvas()
-c.SetLogy()
-data.Draw(argv[1], dataCuts)
-for primitive in c.GetListOfPrimitives():
-    primitive.SetName("data")
-    primitive.SetLineColor(kBlack)
-dataEntries = c.GetPrimitive("data").GetEntries()
-print "data has %i entries" % dataEntries
-sideLow.Draw(argv[2], sidebandCuts, "SAME")
-for primitive in c.GetListOfPrimitives():
-    if not primitive.GetName()=="data":
-        primitive.SetName("sideband")
+    c = TCanvas()
+    c.SetLogy()
+    data.Draw(argv[1], dataCuts)
+
+    primitives = []
+
+    data.Draw(argv[1], dataCuts)
+    for primitive in c.GetListOfPrimitives():
+        primitive.SetName("data")
         primitive.SetBinErrorOption(TH1.kPoisson)
-sidebandEntries = c.GetPrimitive("sideband").GetEntries()
-print "sideband has %i entries" % sidebandEntries
-sidebandNormalization = dataEntries/sidebandEntries
-print "sideband normalization factor is %f" % sidebandNormalization
-for sidebandBin in range (0, c.GetPrimitive("sideband").GetNbinsX()):
-    c.GetPrimitive("sideband").SetBinContent(sidebandBin, c.GetPrimitive("sideband").GetBinContent(sidebandBin)*sidebandNormalization)
-c.GetPrimitive("sideband").SetLineColor(kGreen)
-c.Update()
-c.Draw()
-primitives = []
-for primitive in c.GetListOfPrimitives():
-    primitives.append(primitive.GetName())
-#sig.Draw("higgsJet_pruned_abseta", dataCuts, "SAME")
-#for primitive in c.GetListOfPrimitives():
-#    print primitive
-#    if not primitive.GetName() in primitives:
-#        print "found new primitive with name %s" % primitive.GetName()
-#        primitive.SetName("signal")
-#        print "found updated primitive with name %s" % primitive.GetName()
+        primitive.SetLineColor(kBlack)
+        primitives.append(primitive.GetName())
+    print "\n \n first list of primitives:"
+    for primitive in c.GetListOfPrimitives():
+        print primitive
 
-ymax=0
-for primitive in c.GetListOfPrimitives():
-    if primitive.GetName() in ["sideband", "signal", "data"]:
-        primitive.Rebin(5)
-        if primitive.GetName()=="signal":
-            for sigBin in range(0, c.GetPrimitive("signal").GetNbinsX()):
-                c.GetPrimitive("signal").SetBinContent(sigBin, c.GetPrimitive("signal").GetBinContent(sigBin)*0.1)
-        if primitive.GetMaximum()>ymax:
-            ymax = primitive.GetMaximum()
+    dataEntries = c.GetPrimitive("data").GetEntries()
+    print "data has %i entries" % dataEntries
 
-for primitive in c.GetListOfPrimitives():
-    if primitive.GetName() in ["sideband", "signal", "data"]:
-        primitive.GetYaxis().SetRangeUser(1, ymax*3)
+    sideLow.Draw(argv[2], sidebandCuts, "SAME")
+    for primitive in c.GetListOfPrimitives():
+        if not primitive.GetName()=="data":
+            primitive.SetName("sideband")
+            primitive.SetBinErrorOption(TH1.kPoisson)
+            primitives.append(primitive.GetName())
+    sidebandEntries = c.GetPrimitive("sideband").GetEntries()
+    print "sideband has %i entries" % sidebandEntries
+    sidebandNormalization = dataEntries/sidebandEntries
+    print "sideband normalization factor is %f" % sidebandNormalization
+    for sidebandBin in range (0, c.GetPrimitive("sideband").GetNbinsX()):
+        c.GetPrimitive("sideband").SetBinContent(sidebandBin, c.GetPrimitive("sideband").GetBinContent(sidebandBin)*sidebandNormalization)
+    c.GetPrimitive("sideband").SetLineColor(kGreen)
 
-c.Draw()
-c.Update()
-c2 = TCanvas()
-c2.cd()
-c2.SetLogy()
-c.GetPrimitive("sideband").SetLineColor(kOrange+2)
-c.GetPrimitive("sideband").SetFillColor(kOrange)
-c.GetPrimitive("sideband").Draw()
-clone = c.GetPrimitive("sideband").Clone()
-for sidebandBin in range (0, c.GetPrimitive("sideband").GetNbinsX()):
-    clone.SetBinError(sidebandBin, c.GetPrimitive("sideband").GetBinError(sidebandBin)*sidebandNormalization)
-clone.SetFillColor(kOrange-6)
-clone.SetFillStyle(3017)
-clone.Draw("ZE2 SAME")
-c.GetPrimitive("data").SetMarkerStyle(20)
-c.GetPrimitive("data").Draw("apE0 SAME")
-#c.GetPrimitive("signal").SetLineWidth(3)
-#c.GetPrimitive("signal").SetLineStyle(6)
-#c.GetPrimitive("signal").Draw("SAME")
-c2.Draw()
+    if compareOldCut:
+        dataOld.Draw(argv[1], dataOldCuts, "SAME")
+        for primitive in c.GetListOfPrimitives():
+            if not primitive.GetName() in primitives:
+                primitive.SetName("dataOld")
+                primitive.SetLineColor(kBlack)
+                primitives.append(primitive.GetName())
+        dataOldEntries = c.GetPrimitive("dataOld").GetEntries()
+        print "dataOld has %i entries" % dataOldEntries
 
-outputDirName="output_higgs_sideband%s_masswindow%i-%i"%(sidebandName, massWindowLo, massWindowHi)
-if not path.exists(outputDirName):
-      makedirs(outputDirName)
-outfile = TFile("%s/%s_canvas.root"%(outputDirName, argv[1]),"RECREATE")
-outfile.cd()
-c2.Write()
-outfile.Close()
+        sideLowOld.Draw(argv[2], sidebandOldCuts, "SAME")
+        for primitive in c.GetListOfPrimitives():
+            print "initial:"
+            print primitive
+            if not primitive.GetName() in primitives:
+                primitive.SetName("sidebandOld")
+                primitive.SetBinErrorOption(TH1.kPoisson)
+                primitives.append(primitive.GetName())
+        sidebandOldEntries = c.GetPrimitive("sidebandOld").GetEntries()
+        print "sidebandOld has %i entries" % sidebandOldEntries
+        sidebandOldNormalization = dataOldEntries/sidebandOldEntries
+        print "sidebandOld normalization factor is %f" % sidebandOldNormalization
+        for sidebandOldBin in range (0, c.GetPrimitive("sidebandOld").GetNbinsX()):
+            c.GetPrimitive("sidebandOld").SetBinContent(sidebandOldBin, c.GetPrimitive("sidebandOld").GetBinContent(sidebandOldBin)*sidebandOldNormalization)
+        c.GetPrimitive("sidebandOld").SetLineColor(kRed)
 
-print "\nOutput tcanvas is:\n%s"%outfile.GetName()
-subprocess.call(["python", "tcanvasTDR.py", outfile.GetName(), "-b"])
-exit()
+    print "\n\n List of primitives on c:"
+    for primitive in c.GetListOfPrimitives():
+        print primitive
+
+    if showSignal:
+        sig750.Draw(argv[1], dataCuts, "SAME")
+        for primitive in c.GetListOfPrimitives():
+            if not primitive.GetName() in primitives:
+                print "found new primitive with name %s" % primitive.GetName()
+                primitive.SetName("signal750")
+                print "found updated primitive with name %s" % primitive.GetName()
+                primitives.append(primitive.GetName())
+
+
+        sig1000.Draw(argv[1], dataCuts, "SAME")
+        for primitive in c.GetListOfPrimitives():
+            if not primitive.GetName() in primitives:
+                print "found new primitive with name %s" % primitive.GetName()
+                primitive.SetName("signal1000")
+                print "found updated primitive with name %s" % primitive.GetName()
+                primitives.append(primitive.GetName())
+        print "\n\nhere! the list of primitives is:"
+        print primitives
+        print sig2000
+        sig2000.Draw(argv[1], dataCuts, "SAME")
+        for primitive in c.GetListOfPrimitives():
+            if not primitive.GetName() in primitives:
+                print "found new primitive with name %s" % primitive.GetName()
+                primitive.SetName("signal2000")
+                print "found updated primitive with name %s" % primitive.GetName()
+                primitives.append(primitive.GetName())
+
+
+        sig3000.Draw(argv[1], dataCuts, "SAME")
+        for primitive in c.GetListOfPrimitives():
+            if not primitive.GetName() in primitives:
+                print "found new primitive with name %s" % primitive.GetName()
+                primitive.SetName("signal3000")
+                print "found updated primitive with name %s" % primitive.GetName()
+                primitives.append(primitive.GetName())
+    c.Update()
+    c.Draw()
+    #sig.Draw("higgsJet_pruned_abseta", dataCuts, "SAME")
+    #for primitive in c.GetListOfPrimitives():
+    #    print primitive
+    #    if not primitive.GetName() in primitives:
+    #        print "found new primitive with name %s" % primitive.GetName()
+    #        primitive.SetName("signal")
+    #        print "found updated primitive with name %s" % primitive.GetName()
+
+    ymax=0
+    for primitive in c.GetListOfPrimitives():
+        if primitive.GetName() in ["sideband", "sidebandOld", "signal750",  "signal1000", "signal2000", "signal3000", "data", "dataOld"]:
+            primitive.Rebin(2)
+            if "signal" in primitive.GetName():
+                for sigBin in range(0, c.GetPrimitive(primitive.GetName()).GetNbinsX()):
+                    c.GetPrimitive(primitive.GetName()).SetBinContent(sigBin, c.GetPrimitive(primitive.GetName()).GetBinContent(sigBin)*0.1)
+            if primitive.GetMaximum()>ymax:
+                ymax = primitive.GetMaximum()
+
+    for primitive in c.GetListOfPrimitives():
+        print "final: "
+        print primitive
+        if primitive.GetName() in primitives:
+            primitive.GetYaxis().SetRangeUser(1, ymax*5)
+
+    c.Draw()
+    c.Update()
+    c2 = TCanvas()
+    c2.cd()
+    c2.SetLogy()
+    if compareOldCut:
+        c.GetPrimitive("sideband").SetLineColor(kBlue)
+        c.GetPrimitive("sideband").SetMarkerStyle(20)
+        c.GetPrimitive("sideband").SetMarkerColor(kBlue)
+        c.GetPrimitive("sideband").Draw("pE0")
+        c.GetPrimitive("sidebandOld").SetLineColor(kGray)
+        c.GetPrimitive("sidebandOld").SetFillColor(kOrange)
+        c.GetPrimitive("sidebandOld").SetFillStyle(3004)
+        c.GetPrimitive("sidebandOld").Draw("SAME")
+    else:
+        c.GetPrimitive("sideband").SetLineColor(kOrange+2)
+        c.GetPrimitive("sideband").SetFillColor(kOrange)
+        c.GetPrimitive("sideband").Draw()
+        bgErrs = c.GetPrimitive("sideband").Clone()
+
+        for sidebandBin in range (0, c.GetPrimitive("sideband").GetNbinsX()):
+            bgErrs.SetBinError(sidebandBin, c.GetPrimitive("sideband").GetBinError(sidebandBin)*sidebandNormalization)
+        bgErrs.SetFillColor(kOrange-6)
+        bgErrs.SetFillStyle(3018)
+        bgErrs.Draw("E2 SAME")
+        c.GetPrimitive("data").SetMarkerStyle(20)
+        c.GetPrimitive("data").SetMarkerColor(kBlack)
+        c.GetPrimitive("data").Draw("apE0 SAME")
+    c2.Draw()
+    if showSignal:
+        c.GetPrimitive("signal750").SetLineWidth(3)
+        c.GetPrimitive("signal750").SetLineStyle(6)
+        c.GetPrimitive("signal750").SetLineColor(kBlue)
+        c.GetPrimitive("signal750").Draw("SAME")
+        c.GetPrimitive("signal1000").SetLineWidth(3)
+        c.GetPrimitive("signal1000").SetLineStyle(6)
+        c.GetPrimitive("signal1000").SetLineColor(kGreen)
+        c.GetPrimitive("signal1000").Draw("SAME")
+        c.GetPrimitive("signal2000").SetLineWidth(3)
+        c.GetPrimitive("signal2000").SetLineStyle(6)
+        c.GetPrimitive("signal2000").SetLineColor(kRed)
+        c.GetPrimitive("signal2000").Draw("SAME")
+        c.GetPrimitive("signal3000").SetLineWidth(3)
+        c.GetPrimitive("signal3000").SetLineStyle(6)
+        c.GetPrimitive("signal3000").SetLineColor(kCyan)
+        c.GetPrimitive("signal3000").Draw("SAME")
+    c2.Draw()
+    outputDirName="output_Z_sideband%s_masswindow%i-%i"%(sidebandName, massWindowLo, massWindowHi)
+    if not path.exists(outputDirName):
+          makedirs(outputDirName)
+    outfile = TFile("%s/%s_canvas.root"%(outputDirName, argv[1]),"RECREATE")
+    outfile.cd()
+
+    c.GetPrimitive("sideband").Clone().Write()
+    if compareOldCut:
+        c.GetPrimitive("sidebandOld").Clone().Write()
+    c2.Write()
+    outfile.Close()
+
+    print "\nOutput tcanvas is:\n%s"%outfile.GetName()
+    subprocess.call(["python", "tcanvasTDR.py", outfile.GetName(), "-b"])
+    c2.Update()
+    c2.Draw()
+    return c2
