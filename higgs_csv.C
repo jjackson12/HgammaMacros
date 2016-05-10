349cc4fa6be06a1496b37150aad122981c38f795
John Hakala <john_hakala@brown.edu>
2016-05-10 21:12:03 +0200

    modify scripts for checking subjet csv cuts

--- higgs_csv.C
+++ higgs_csv.C
@@ -1,10 +1,10 @@
-#define higgs_cxx
+#define higgs_csv_cxx
 #include <iostream>
 #include "TMath.h"
-#include "higgs.h"
+#include "higgs_csv.h"
 #include "treeChecker.h"
 
-int higgs::Loop(float cutValue, float lowerMassBound, float upperMassBound)
+int higgs_csv::Loop(string workingpoint, float lowerMassBound, float upperMassBound)
 {
    if (fChain == 0) return -1;
    Long64_t nentries = fChain->GetEntriesFast();
@@ -16,15 +16,26 @@ int higgs::Loop(float cutValue, float lowerMassBound, float upperMassBound)
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
       if (  Cut(ientry) < 0 
-         || higgsJet_HbbTag <= cutValue 
          || phJetInvMass_pruned_higgs < lowerMassBound 
          || phJetInvMass_pruned_higgs > upperMassBound ) continue;
+      if (workingpoint == "looseloose"
+          && !loose_loose) continue;
+      if (workingpoint == "mediumloose"
+          && !medium_loose) continue;
+      if (workingpoint == "tightloose"
+          && !tight_loose) continue;
+      if (workingpoint == "mediummedium"
+          && !medium_medium) continue;
+      if (workingpoint == "tightmedium"
+          && !tight_medium) continue;
+      if (workingpoint == "tighttight"
+          && !tight_tight) continue;
       ++nHiggsJets;
    }
    return nHiggsJets;
 }
 
-Int_t higgs::Cut(Long64_t entry) {
+Int_t higgs_csv::Cut(Long64_t entry) {
   if (   cosThetaStar<-0.627*TMath::ATan((-0.005938*phJetInvMass_pruned_higgs)+3.427)
      &&  leadingPhAbsEta          < 1.4442
      &&  higgsJet_pruned_abseta   < 2.2
