9e4ea1feec59424869cdfdd202d82e8688e7a285
John Hakala <john_hakala@brown.edu>
2016-07-08 20:28:17 -0400

    fix up stackplot tools

--- HgCuts.py
+++ HgCuts.py
@@ -19,26 +19,40 @@ def combineCuts(cutDict):
     combinedCut += cutDict[cut]
   return combinedCut
 
-def getDefaultCuts():
+def getDefaultCuts(region):
     cutValues = getCutValues()
 
     cuts = {} 
-    cuts["turnon"]   = TCut( "phJetInvMass_pruned_higgs>%f" % cutValues["minInvMass"] )
     cuts["phEta"]    = TCut( "leadingPhAbsEta<%f"           % cutValues["photonEta"]  )
-    cuts["jetEta"]   = TCut( "higgsJet_pruned_abseta<%f"    % cutValues["jetEta"]     )
-    cuts["deltaR"]   = TCut( "phJetDeltaR_higgs>%f"         % cutValues["deltaR"]     )
     cuts["ptOverM"]  = TCut( "phPtOverMgammaj>%f"           % cutValues["ptOverM"]    )
-    cuts["btag"]     = TCut( "higgsJet_HbbTag>%f"           % cutValues["Hbb"]        )
-    cuts["antibtag"] = TCut( "higgsJet_HbbTag<%f"           % cutValues["Hbb"]        )
+    if region is "higgs":
+      cuts["turnon"]   = TCut( "phJetInvMass_pruned_higgs>%f" % cutValues["minInvMass"] )
+      cuts["deltaR"]   = TCut( "phJetDeltaR_higgs>%f"         % cutValues["deltaR"]     )
+      cuts["jetEta"]   = TCut( "higgsJet_pruned_abseta<%f"    % cutValues["jetEta"]     )
+      cuts["btag"]     = TCut( "higgsJet_HbbTag>%f"           % cutValues["Hbb"]        )
+      cuts["antibtag"] = TCut( "higgsJet_HbbTag<%f"           % cutValues["Hbb"]        )
+    elif region is "side5070" or "side100110":
+      if region is "side5070":
+        index = "Three"
+      else:
+        index = "Four"
+      cuts["turnon"]   = TCut( "phJetInvMass_pruned_sideLow%s>%f" % (index, cutValues["minInvMass"] ))
+      cuts["deltaR"]   = TCut( "phJetDeltaR_sideLow%s>%f"         % (index, cutValues["deltaR"]     ))
+      cuts["jetEta"]   = TCut( "sideLow%sJet_pruned_abseta<%f"    % (index, cutValues["jetEta"]     ))
+      cuts["btag"]     = TCut( "sideLow%sJet_HbbTag>%f"           % (index, cutValues["Hbb"]        ))
+      cuts["antibtag"] = TCut( "sideLow%sJet_HbbTag<%f"           % (index, cutValues["Hbb"]        ))
+    else:
+      print "Invalid region!!!"
+      quit()
     return cuts
     
-def getBtagComboCut():
-    btagCuts = copy.deepcopy(getDefaultCuts())
+def getBtagComboCut(region):
+    btagCuts = copy.deepcopy(getDefaultCuts(region))
     btagCuts.pop("antibtag")
     return combineCuts(btagCuts)
     
-def getAntiBtagComboCut():
-    antibtagCuts = copy.deepcopy(getDefaultCuts())
+def getAntiBtagComboCut(region):
+    antibtagCuts = copy.deepcopy(getDefaultCuts(region))
     antibtagCuts.pop("btag")
     return combineCuts(antibtagCuts)
 
--- testpy.py
+++ testpy.py
@@ -7,12 +7,12 @@ from HgCuts import *
 sampleDirs = getSamplesDirs()
 weightsDict = getWeightsDict(sampleDirs["small3sDir"])
 
-def makeHist(hist, var, key, region):
-  nEntries = tree.Draw("%s>> hist"%var, getAntiBtagComboCut())
+def makeHist(tree, hist, var, key, region):
+  nEntries = tree.Draw("%s>> hist"%var, getAntiBtagComboCut(region))
   if nEntries == 0:
     return False
   else:
-    outFile = TFile("weightedMCbgHists/%s_%s_%s"%(region, var, key), "RECREATE")
+    outFile = TFile("weightedMCbgHists/%s_%s_%s"%(key, region, var), "RECREATE")
     outFile.cd()
     for histBin in range (0,hist.GetXaxis().GetNbins()):
       hist.SetBinContent(histBin, hist.GetBinContent(histBin)*weightsDict[key])  
@@ -21,22 +21,21 @@ def makeHist(hist, var, key, region):
     return True
   
 pre = getFilePrefix()
-varNames = []
-tfile = TFile(sampleDirs["ddDir"]+pre+getWeightsDict(getSamplesDirs()["small3sDir"]).keys()[0])
-sidebandTree = tfile.Get("side100110")
-for branch in sidebandTree.GetListOfBranches():
-  varNames.append(branch.GetName())
 regions = ["higgs", "side100110", "side5070"]
-for region in regions:
-  tree = tfile.Get(region)
-  for var in varNames:
-    for key in getWeightsDict(getSamplesDirs()["small3sDir"]).keys():
-      tfile = TFile(sampleDirs["ddDir"]+pre+key)
-      sidebandTree = tfile.Get("side100110")
-      #fullFileName = sampleDirs["ddDir"]+pre+weightsDict.keys()[1]
-      hist = TH1F("hist","hist",3250,0,13000)
-      if makeHist(hist, var, key, region):
-    
+for key in getWeightsDict(getSamplesDirs()["small3sDir"]).keys():
+  tfile = TFile(sampleDirs["ddDir"]+pre+key)
+  for region in regions:
+    inTree = tfile.Get(region)
+    varNames = []
+    for branch in inTree.GetListOfBranches():
+      varNames.append(branch.GetName())
+    print "Tree name %s has branches:"%inTree.GetName()
+    print varNames
+    for var in varNames:
+      hist = TH1F("hist_%s_%s_%s"%(key, var, region),"hist_%s_%s_%s"%(key, var, region),3250,0,13000)
+      if makeHist(inTree, hist, var, key, region):
+
         cblah=TCanvas()
         cblah.cd()
         hist.Draw()
+    del varNames
