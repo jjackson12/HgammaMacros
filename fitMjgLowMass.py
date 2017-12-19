591795546a1d70d1119951cae0471a38a719c91e
John Hakala <john_hakala@brown.edu>
2017-12-19 17:54:14 -0500

    write solution on plot

--- fitMjgLowMass.py
+++ fitMjgLowMass.py
@@ -8,12 +8,11 @@ from HgCuts import *
 
 can = TCanvas()
 can.cd()
-gStyle.SetOptFit()
 
 graph = TGraph()
 
 useWeightFactors=True
-outDir = "turnOns"
+outDir = "turnOns_functions_"
 if useWeightFactors:
   outDir += "_withWeightFactors"
 else:
@@ -54,7 +53,22 @@ for jetMassWindow in jetMassWindows:
     result = hist.Fit(fit, "SLQ", "", 500, 2500)
     print "result of fit, second try:", result.IsValid()
    
-  can.Print(path.join(outDir, "turnOn_%i-%i.pdf" % (int(jetMassWindow[0]), int(jetMassWindow[1]))))
+
+  erfComponent = TF1("erf", "[0]*(0.5+0.5*TMath::Erf((x-[1])/[2]))", 0, 13000)
+  erfComponent.SetParameters(fit.GetMaximum(), fit.GetParameter(3), fit.GetParameter(4))
+  erfComponent.SetLineColor(kGray)
+  erfComponent.SetLineStyle(7)
+  expComponent = TF1("exp", "[0]*TMath::Exp([1]*x-[2])", 0, 13000)
+  expComponent.SetParameters(fit.GetParameter(0), fit.GetParameter(1), fit.GetParameter(2))
+  expComponent.SetLineColor(kGray+2)
+  expComponent.SetLineStyle(7)
+  erfComponent.Draw("SAME")
+  expComponent.Draw("SAME")
+  gStyle.SetOptFit(0)
+  gStyle.SetOptStat(0)
+  #for prim in can.GetListOfPrimitives():
+  #  if hasattr(prim, 'GetName'):
+  #    print prim.GetName()
   print "solution for mass window", jetMassWindow, ":"
   solution = solve(-0.99 + 0.5 + 0.5*(erf((x-fit.GetParameter(3))/fit.GetParameter(4))), x)
   print solution
@@ -64,6 +78,14 @@ for jetMassWindow in jetMassWindows:
   else:
     print "... did not get exactly one solution for this mass window"
   turnOns.append(([jetMassWindow[0],jetMassWindow[1]], solution[0]))
+  line = TLine(solution[0], 0, solution[0], can.GetFrame().GetY2())
+  line.SetLineColor(kBlack)
+  line.Draw("SAME")
+  text = TPaveText(solution[0]+can.GetFrame().GetX2()/16., can.GetFrame().GetY2()/12., solution[0]+can.GetFrame().GetX2()/16. , can.GetFrame().GetY2()/12.)
+  text.AddText("m_{j\gamma}=%01d GeV" % solution[0])
+  text.SetTextSize(0.03)
+  text.Draw("SAME")
+  can.Print(path.join(outDir, "turnOn_%i-%i.pdf" % (int(jetMassWindow[0]), int(jetMassWindow[1]))))
   
 
 can2=TCanvas()
